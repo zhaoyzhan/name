a72ccd03f50e5fdf3e0f3d61d5c9efcec49b537c
zhaozhan <1048771638@qq.com>
2017-12-28 19:17:00 +0800

    12/28

---
+++ src/components/compones/DiGuiCom/DiGui.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mainC">
+    <ul>
+      <DiGui :tree="tree"></DiGui>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import DiGui from './children/DiGuiChildren'
+  export default {
+    data () {
+      return {
+        tree: [{
+          label: 'A第一级',
+          children: [{
+            label: 'A第二级',
+            children: [{
+              label: 'A第三级'
+            }]
+          }]
+        }, {
+          label: 'B第一级',
+          children: [{
+            label: 'B第二级',
+            children: [{
+              label: 'B第三级'
+            }]
+          }, {
+            label: 'B2第二级',
+            children: [{
+              label: 'b2第三级'
+            }]
+          }]
+        }]
+      }
+    },
+    compones: {
+      DiGui
+    }
+  }
+</script>
+
+<script>
+	
+</script>
---
+++ src/components/compones/DiGuiCom/children/DiGuiChildren.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="con">
+  	<li>
+  	  <a href="">{{tree.label}}</a>
+  	  <ul>
+  	  	<diguiChild v-if="tree.children" :tree="item" v-for="item in tree.children"></diguiChild>
+  	  </ul>
+  	</li>
+  </div>
+</template>
+
+<script>
+  export default {
+  	name: 'diguiChild',
+  	props: ['tree']
+  }
+</script>
+
+<script>
+	
+</script>
